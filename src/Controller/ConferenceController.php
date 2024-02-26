2b908c7c2754c9355cd350bf91b95b480cecf558
VascoII <fabriceds@yahoo.fr>
2024-02-26 15:53:17 +0100

    Add: Controller, Entity, Repository and migrations file

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,14 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
